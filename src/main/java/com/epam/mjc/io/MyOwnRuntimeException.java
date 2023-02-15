a3f89059a2599a50ed78eecb6af8c720df84c595
Jahongir2109 <jahongirachilov004@gmail.com>
2023-02-15 18:13:51 +0500

    fixed custom exception

--- src/main/java/com/epam/mjc/io/MyOwnRuntimeException.java
+++ src/main/java/com/epam/mjc/io/MyOwnRuntimeException.java
@@ -1,7 +1,4 @@
 package com.epam.mjc.io;
-
-import java.io.FileNotFoundException;
-
 public class MyOwnRuntimeException extends RuntimeException{
     public MyOwnRuntimeException(String message) {
         super(message);
