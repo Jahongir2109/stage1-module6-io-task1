227f0b70e0bca3978338fa7ac1ec6ade5150256b
Jahongir2109 <jahongirachilov004@gmail.com>
2023-02-15 16:56:50 +0500

    added double catch block

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,29 +7,22 @@ import java.util.Scanner;
 
 
 public class FileReader {
-    public Profile getDataFromFile(File file){
+    public Profile getDataFromFile(File file) {
         Map map=new HashMap();
         String str;
         BufferedReader bufferedReader= null;
         try {
             bufferedReader = new BufferedReader(new java.io.FileReader(file.getAbsolutePath()));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(bufferedReader);
-            while (true) {
-                try {
-                    if (!bufferedReader.ready()) break;
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    str = bufferedReader.readLine();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+            while (bufferedReader.ready()) {
+                str = bufferedReader.readLine();
                 map.put(str.substring(0,str.indexOf(": ")),str.substring(str.indexOf(": ")+2));
             }
-        return new Profile( map.get("Name").toString(), Integer.valueOf(map.get("Age").toString()), (String) map.get("Email"), (Long) map.get("Phone"));
+            return new Profile( map.get("Name").toString(), Integer.valueOf(map.get("Age").toString()), (String) map.get("Email"), Long.valueOf((String) map.get("Phone")));
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
