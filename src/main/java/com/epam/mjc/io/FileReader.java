195616d426a6ae59c20881757385a293d0121c61
Jahongir2109 <jahongirachilov004@gmail.com>
2023-02-15 18:10:01 +0500

    clean code conception fixed again

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 public class FileReader {
     public Profile getDataFromFile(File file) {
-        Map<String,String> map=new HashMap();
+        Map<String,String> map = null;
         try(BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file.getAbsolutePath()));){
             String str;
             while (bufferedReader.ready()) {
@@ -13,7 +13,7 @@ public class FileReader {
             }
             return new Profile( map.get("Name"), Integer.valueOf(map.get("Age")), map.get("Email"), Long.valueOf(map.get("Phone")));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new MyOwnRuntimeException(e.getMessage());
         }
     }
 }
--- src/main/java/com/epam/mjc/io/MyOwnRuntimeException.java
+++ src/main/java/com/epam/mjc/io/MyOwnRuntimeException.java
@@ -1,10 +1,9 @@
 package com.epam.mjc.io;
 
+import java.io.FileNotFoundException;
+
 public class MyOwnRuntimeException extends RuntimeException{
     public MyOwnRuntimeException(String message) {
         super(message);
-    }
-    public void foo(String bar) throws Throwable {  // Noncompliant
-        throw new RuntimeException("My Message");     // Noncompliant
     }
 }
